95810d0ab6208743f674df300197133d72c3abb2
Denis Khasanov <denishasanov828@gmail.com>
2025-04-19 01:10:54 +0500

    0.1

--- RocketGame/Assets/CodeLegacy/AustralianSpider/Scripts/ScriptableObjects/Bet Amounts/BetAmounts.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BetAmounts", menuName = "Slots/Bet amounts")]
-public class BetAmounts : ScriptableObject
-{
-    public List<int> betValues = new List<int>();
-}
--- RocketGame/Assets/CodeLegacy/AustralianSpider/Scripts/ScriptableObjects/MiniGameCoinNominals/MiniGameCoinNominals.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CoinNominals", menuName = "Mini game/Coin nominals")]
-public class MiniGameCoinNominals : ScriptableObject
-{
-    public List<int> Nominals = new List<int>();
-}
